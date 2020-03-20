b5b6738a725dcdb41bdf30da962183aa807f7301
userRND <userRND@idino.co.kr>
2020-03-20 15:12:07 +0900

    수정

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
+import axios from "axios";
+
+const API_KEY = "6f461c910bffcac29a7e82f88121620e";
 
 export default class extends React.Component {
   state = {
     isLoading: true
+  }
+  getWeather = async(latitude, longitude) => {
+    const {data} = await axios.get(
+      `api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      );
   }
   getLocation = async() => {
     try{
@@ -13,6 +21,7 @@ export default class extends React.Component {
       await Location.requestPermissionsAsync();      
       const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
       // Send to API and get weather
+      this.getWeather(latitude, longitude);
       this.setState({isLoading: false});
     } catch (error) {
       Alert.alert("Can't find you.");
