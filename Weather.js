b611fa97201228eef2024ce173fc900ffe0533a7
userRND <userRND@idino.co.kr>
2020-03-20 16:36:47 +0900

    수정

--- Weather.js
+++ Weather.js
@@ -4,15 +4,61 @@ import PropTypes from "prop-types";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Weather ({temp}){
+const weatherOptions = {
+    Haze:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Thunderstorm:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Dizzle:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Rain:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Snow:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Atmosphere:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Clear:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Clouds:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Mist:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    },
+    Dust:{
+        iconName:"weather-hail",
+        gradient:["#4DA0b0","#D39D38"]
+    }
+}
+
+export default function Weather ({temp, condition}){
     return (
-        
             <LinearGradient
-                colors={['#4c669f', '#3b5998', '#192f6a']}
+                colors={weatherOptions["Haze"].gradient}
                 style={styles.container}>
                 <StatusBar barStyle="light-content"></StatusBar>
                 <View style={styles.halfcontainer}>
-                    <MaterialCommunityIcons size={96} name="weather-lightning-rainy" color="white"></MaterialCommunityIcons>
+                    <MaterialCommunityIcons 
+                        size={96} 
+                        name={weatherOptions["Haze"].iconName} 
+                        color="white">
+                    </MaterialCommunityIcons>
                     <Text style={styles.temp}>{temp}</Text>
                 </View>
                 <View style={styles.halfcontainer}></View>
