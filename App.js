f7f6231523c5c9a160a7a0664611ac9528b0fe5e
userRND <userRND@idino.co.kr>
2020-03-20 14:42:40 +0900

    Init2

--- App.js
+++ App.js
@@ -4,12 +4,16 @@ import * as Location from 'expo-location';
 import { Alert } from 'react-native';
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
   getLocation = async() => {
     try{
-      const response = await Location.requestPermissionsAsync();
-      console.log(response);
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+
+      await Location.requestPermissionsAsync();      
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      // Send to API and get weather
+      this.setState({isLoading: false});
     } catch (error) {
       Alert.alert("Can't find you.");
     }
@@ -18,7 +22,8 @@ export default class extends React.Component {
     this.getLocation();
   }
   render(){
-    return <Loading></Loading>;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading></Loading> : null;
   }
 }
 
