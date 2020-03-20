4262671c14f1369493f3bc750eb1e0dbe993cc89
userRND <userRND@idino.co.kr>
2020-03-20 15:58:35 +0900

    수정

--- Weather.js
+++ Weather.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Weather ({temp}){
     return (
         <View style={styles.container}>
-            <Text>{temp}</Text>
+            <View style={styles.halfcontainer}>
+                <MaterialCommunityIcons size={96} name="weather-lightning-rainy"></MaterialCommunityIcons>
+                <Text style={styles.temp}>{temp}</Text>
+            </View>
+            <View style={styles.halfcontainer}></View>
         </View>
     );
 }
 
 Weather.propTypes = {
-    temp:PropTypes.number.isRequired
+    temp:PropTypes.number.isRequired,
+    condition: PropTypes.oneOf(["Thunderstorm", "Dizzle", "Rain", "Snow"
+    , "Atmosphere", "Clear", "Clouds","Haze","Mist","Dust"]).isRequired
 }
 
 const styles = StyleSheet.create({
     container:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp:{
+        fontSize:42
+    },
+    halfcontainer:{
         flex:1,
         justifyContent: "center",
         alignItems: "center"
